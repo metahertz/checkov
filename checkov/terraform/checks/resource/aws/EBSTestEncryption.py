079aea298d5f69d665afda49bf7ba1ea82cb3ee9
Matt Johnson <matt@bridgecrew.io>
2020-09-12 16:53:16 +0100

    new test for diff

---
+++ checkov/terraform/checks/resource/aws/EBSTestEncryption.py
@@ -0,0 +1,17 @@
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.enums import CheckCategories
+
+
+class EBSTestEncryption(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure this test is new so we can diff [TESTING ONLY - NOT FOR PROD]"
+        id = "CKV_AWS_3000"
+        supported_resources = ['aws_ebs_volume']
+        categories = [CheckCategories.ENCRYPTION]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return "encrypted"
+
+
+check = EBSTestEncryption()
